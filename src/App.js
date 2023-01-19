5ad9802e9e39d4c15bb93cbedd03b7c18ce1e0f5
DagmaraJaneczek <janeczek.dagmara@gmail.com>
2023-01-19 15:58:27 +0100

    Add db folder and crate server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/views/Header/Header";
 import Footer from "./components/views/Footer/Footer";
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home/Home";
 
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
       <Container>
         <Header />
         <Routes>
-
+          <Route path="/" element={<Home />} />
         </Routes>
         <Footer />
       </Container>
