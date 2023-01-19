39cda27aae4cdfbb77d9588c3e54b630daa5aa27
DagmaraJaneczek <janeczek.dagmara@gmail.com>
2023-01-19 17:57:51 +0100

    Add NotFound page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/views/Footer/Footer";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import Table from "./components/pages/Table/Table";
+import NotFound from "./components/views/NotFound/NotFound";
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/table/:id" element={<Table />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Container>
