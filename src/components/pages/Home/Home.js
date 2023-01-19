4e3ee09b3da48d494ad644d8802f82ae29510780
DagmaraJaneczek <janeczek.dagmara@gmail.com>
2023-01-19 16:54:23 +0100

    Add Home page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,6 +1,46 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Stack, Button } from 'react-bootstrap';
+
+
 const Home = () => {
     return(
-        <h1>Home</h1>
+        <div>
+          <h1 className='my-3'>All tables</h1>
+          <ListGroup variant='flush'>
+            <ListGroup.Item className='px-0'>
+              <Stack direction="horizontal" gap={3}>
+                <h3 >Table 1</h3>
+                <p className='mb-0'><strong>Status:</strong></p>
+                <Button variant='primary' className="ms-auto">Show more</Button>
+              </Stack>
+            </ListGroup.Item>
+
+            <ListGroup.Item className='px-0'>
+            <Stack direction="horizontal" gap={3}>
+                <h3>Table 2</h3>
+                <p className='mb-0'><strong>Status:</strong></p>
+                <Button variant='primary' className="ms-auto">Show more</Button>
+              </Stack>
+            </ListGroup.Item>
+
+            <ListGroup.Item className='px-0'>
+            <Stack direction="horizontal" gap={3}>
+                <h3>Table 3</h3>
+                <p className='mb-0'><strong>Status:</strong></p>
+                <Button variant='primary' className="ms-auto">Show more</Button>
+              </Stack>
+            </ListGroup.Item>
+
+            <ListGroup.Item className='px-0'>
+            <Stack direction="horizontal" gap={3}>
+                <h3>Table 4</h3>
+                <p className='mb-0'><strong>Status:</strong></p>
+                <Button variant='primary' className="ms-auto">Show more</Button>
+              </Stack>
+            </ListGroup.Item>
+          </ListGroup>
+        </div>
+
     )
 };
 
